df263b00a5c384bf3bb12c23e7e54ce12223badc
alex <Sagayda4niyAlexey@gmail.com>
2017-01-06 14:30:41 +0200

    Clear one char button added

--- app/src/main/java/com/example/alex/balance/BalanceFragment.java
+++ app/src/main/java/com/example/alex/balance/BalanceFragment.java
@@ -9,22 +9,42 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import net.cachapa.expandablelayout.ExpandableLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * Created by alex on 04.01.17.
  */
 
 public class BalanceFragment extends Fragment implements View.OnClickListener {
-    private ExpandableLayout mKeyboardExpand, mEditExpand;
-    private ImageView mIvKeyboard, mIvEdit;
-    private RelativeLayout mRlKeyboard, mRlEdit;
+    @BindView(R.id.total_sum_tv)
+    TextView mTvTotalSum;
+    @BindView(R.id.keyboard_expand)
+    ExpandableLayout mKeyboardExpand;
+    @BindView(R.id.edit_expand)
+    ExpandableLayout mEditExpand;
+    @BindView(R.id.image_keyboard_button)
+    ImageView mIvKeyboard;
+    @BindView(R.id.image_notes_button)
+    ImageView mIvEdit;
+    @BindView(R.id.keyboard_button)
+    RelativeLayout mRlKeyboard;
+    @BindView(R.id.notes_button)
+    RelativeLayout mRlEdit;
+    @BindView(R.id.del_one_button)
+    RelativeLayout mRlDelOne;
+
     private int[] mSwitchButtons = {
             R.id.category_button,
             R.id.keyboard_button,
             R.id.notes_button
     };
+    private Unbinder mUnbinder;
 
     @Nullable
     @Override
@@ -35,16 +55,10 @@ public class BalanceFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        mUnbinder = ButterKnife.bind(this, view);
 
-        mKeyboardExpand = (ExpandableLayout) view.findViewById(R.id.keyboard_expand);
-        mEditExpand = (ExpandableLayout) view.findViewById(R.id.edit_expand);
-
-        mIvKeyboard = (ImageView) view.findViewById(R.id.image_keyboard_button);
-        mIvEdit = (ImageView) view.findViewById(R.id.image_notes_button);
-
-        mRlKeyboard = (RelativeLayout) view.findViewById(R.id.keyboard_button);
-        mRlEdit = (RelativeLayout) view.findViewById(R.id.notes_button);
-
+        mRlDelOne.setOnClickListener(this);
+        view.findViewById(R.id.button_cancel).setOnClickListener(this);
         for (int i : mSwitchButtons) {
             view.findViewById(i).setOnClickListener(this);
         }
@@ -53,6 +67,16 @@ public class BalanceFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
+            case R.id.del_one_button:
+                String totalSumTxt = mTvTotalSum.getText().toString();
+                if (totalSumTxt.length() == 1) {
+                    mTvTotalSum.setText("0");
+                } else if (totalSumTxt.charAt(totalSumTxt.length() - 2) == '.') {
+                    mTvTotalSum.setText(totalSumTxt.substring(0, totalSumTxt.length() - 2));
+                } else {
+                    mTvTotalSum.setText(totalSumTxt.substring(0, totalSumTxt.length() - 1));
+                }
+                break;
             case R.id.category_button:
                 break;
             case R.id.keyboard_button:
@@ -66,6 +90,9 @@ public class BalanceFragment extends Fragment implements View.OnClickListener {
                     expand(mEditExpand);
                     activateButton(mIvEdit, mRlEdit, getActivity().getResources().getDrawable(R.drawable.ic_underline_button));
                 }
+                break;
+            case R.id.button_cancel:
+                getActivity().onBackPressed();
                 break;
         }
     }
@@ -85,5 +112,11 @@ public class BalanceFragment extends Fragment implements View.OnClickListener {
         mKeyboardExpand.collapse();
         mEditExpand.collapse();
         expand.expand();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mUnbinder.unbind();
     }
 }
